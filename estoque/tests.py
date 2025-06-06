90232c08bac6ede7e21503fe3b74be7241a08bfc
Eric Rafael <ericrafael753@gmail.com>
2025-06-06 16:15:21 -0300

    Add basic tests for Ativo model and list view

--- estoque/tests.py
+++ estoque/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Ativo
+
+
+class AtivoModelTests(TestCase):
+    def test_ativo_str_representation(self):
+        ativo = Ativo.objects.create(
+            tipo="Computador",
+            nome="Dell",
+            numero_patrimonio="ABC123",
+            status="disponível",
+        )
+        self.assertEqual(str(ativo), "Computador - Dell (ABC123)")
+
+
+class AtivoListViewTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="tester", password="testpass")
+
+    def test_list_view_authenticated(self):
+        self.client.login(username="tester", password="testpass")
+        response = self.client.get(reverse("ativo_list"))
+        self.assertEqual(response.status_code, 200)
